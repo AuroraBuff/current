7b0e11f654e1c1746cd7c5b2652c7a7ebabdf2da
zhengzhoule <1453250595@qq.com>
2023-11-14 17:08:44 +0800

    build:alpha 2023-11-14 17:08:44

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,20 @@
 echo "git push current"
+git pull
 
-pnpm install
-pnpm build&
-wait
+if [ $? -ne 0 ]; then
+  echo "git pull 命令出现错误，终止脚本的执行"
+  exit 1
+fi
+
+pnpm build:$1
+if [ $? -ne 0 ]; then
+  echo "pnpm build 命令出现错误，终止脚本的执行"
+  exit 1
+fi
+
+echo "构建完毕，准备提交到远程仓库"
+current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
 git add .
-git commit -m "update"
-git push -u origin master
+git commit -m "build:$1 $current_datetime"
+git push
